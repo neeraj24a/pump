457602c0fe657d223aa678d073bf9c2754579ab0
neeraj24a <neeraj24a@gmail.com>
2017-02-08 17:16:32 +0530

    Expense Update View

--- protected/modules/admin/views/expenses/update.php
+++ protected/modules/admin/views/expenses/update.php
@@ -1,21 +1,23 @@
-<?php
-/* @var $this ExpensesController */
-/* @var $model Expenses */
-
-$this->breadcrumbs=array(
-	'Expenses'=>array('index'),
-	$model->id=>array('view','id'=>$model->id),
-	'Update',
-);
-
-$this->menu=array(
-	array('label'=>'List Expenses', 'url'=>array('index')),
-	array('label'=>'Create Expenses', 'url'=>array('create')),
-	array('label'=>'View Expenses', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage Expenses', 'url'=>array('admin')),
-);
-?>
-
-<h1>Update Expenses <?php echo $model->id; ?></h1>
-
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<section class="content-header">
+  <h1>
+    Update
+    <small>Expense</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="<?php echo base_url() . '/admin/dashboard'; ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+    <li><a href="<?php echo base_url() . '/admin/expenses'; ?>"><i class="fa fa-dashboard"></i> Expenses</a></li>
+    <li class="active">Update</li>
+</ol>
+</section>
+<section class="content">
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="box box-info">
+				<div class="box-header with-border">
+					<h3 class="box-title"><?php echo $model->expense_date; ?></h3>
+				</div>
+				<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+			</div>
+		</div>
+	</div>
+</section>
