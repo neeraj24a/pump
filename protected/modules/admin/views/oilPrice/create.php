51b2cd50023fdf7abf9dd0ecfe89beb7f8407b56
neeraj24a <neeraj24a@gmail.com>
2017-02-08 17:10:50 +0530

    Oil Price Create View

--- protected/modules/admin/views/oilPrice/create.php
+++ protected/modules/admin/views/oilPrice/create.php
@@ -1,18 +1,23 @@
-<?php
-/* @var $this OilPriceController */
-/* @var $model OilPrice */
-
-$this->breadcrumbs=array(
-	'Oil Prices'=>array('index'),
-	'Create',
-);
-
-$this->menu=array(
-	array('label'=>'List OilPrice', 'url'=>array('index')),
-	array('label'=>'Manage OilPrice', 'url'=>array('admin')),
-);
-?>
-
-<h1>Create OilPrice</h1>
-
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<section class="content-header">
+  <h1>
+    Add
+    <small>Oil Price</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="<?php echo base_url() . '/admin/dashboard'; ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+    <li><a href="<?php echo base_url() . '/admin/oilPrice'; ?>"><i class="fa fa-dashboard"></i> Oil Price</a></li>
+    <li class="active">Add</li>
+</ol>
+</section>
+<section class="content">
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="box box-info">
+				<div class="box-header with-border">
+					<h3 class="box-title">Oil Price</h3>
+				</div>
+				<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+			</div>
+		</div>
+	</div>
+</section>
