b2a75a4df586046127b676a2f79f92aaba71ba25
neeraj24a <neeraj24a@gmail.com>
2017-02-08 17:51:06 +0530

    Commission Create Update

--- protected/modules/admin/views/commission/create.php
+++ protected/modules/admin/views/commission/create.php
@@ -1,18 +1,23 @@
-<?php
-/* @var $this CommissionController */
-/* @var $model Commission */
-
-$this->breadcrumbs=array(
-	'Commissions'=>array('index'),
-	'Create',
-);
-
-$this->menu=array(
-	array('label'=>'List Commission', 'url'=>array('index')),
-	array('label'=>'Manage Commission', 'url'=>array('admin')),
-);
-?>
-
-<h1>Create Commission</h1>
-
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<section class="content-header">
+  <h1>
+    Add
+    <small>Commission</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="<?php echo base_url() . '/admin/dashboard'; ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+    <li><a href="<?php echo base_url() . '/admin/commission'; ?>"><i class="fa fa-dashboard"></i> Commission</a></li>
+    <li class="active">Add</li>
+</ol>
+</section>
+<section class="content">
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="box box-info">
+				<div class="box-header with-border">
+					<h3 class="box-title">Commission</h3>
+				</div>
+				<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+			</div>
+		</div>
+	</div>
+</section>
