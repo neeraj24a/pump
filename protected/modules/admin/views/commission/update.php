ce0385a52a1e51b6f66cb4a9293c5829d4b6153d
neeraj24a <neeraj24a@gmail.com>
2017-02-08 17:53:13 +0530

    Commission Update View

--- protected/modules/admin/views/commission/update.php
+++ protected/modules/admin/views/commission/update.php
@@ -1,21 +1,23 @@
-<?php
-/* @var $this CommissionController */
-/* @var $model Commission */
-
-$this->breadcrumbs=array(
-	'Commissions'=>array('index'),
-	$model->id=>array('view','id'=>$model->id),
-	'Update',
-);
-
-$this->menu=array(
-	array('label'=>'List Commission', 'url'=>array('index')),
-	array('label'=>'Create Commission', 'url'=>array('create')),
-	array('label'=>'View Commission', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage Commission', 'url'=>array('admin')),
-);
-?>
-
-<h1>Update Commission <?php echo $model->id; ?></h1>
-
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<section class="content-header">
+  <h1>
+    Update
+    <small>Commission</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="<?php echo base_url() . '/admin/dashboard'; ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+    <li><a href="<?php echo base_url() . '/admin/commission'; ?>"><i class="fa fa-dashboard"></i> Commission</a></li>
+    <li class="active">Update</li>
+</ol>
+</section>
+<section class="content">
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="box box-info">
+				<div class="box-header with-border">
+					<h3 class="box-title"><?php echo ProductType::model()->findByPk($model->product_type)->name; ?></h3>
+				</div>
+				<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+			</div>
+		</div>
+	</div>
+</section>
