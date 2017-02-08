beb43fa233ca3e99da05bddaa457155c0d94c1ce
neeraj24a <neeraj24a@gmail.com>
2017-02-08 17:30:04 +0530

    Expense admin View Update

--- protected/modules/admin/views/expenses/admin.php
+++ protected/modules/admin/views/expenses/admin.php
@@ -1,63 +1,98 @@
-<?php
-/* @var $this ExpensesController */
-/* @var $model Expenses */
+<section class="content-header">
+    <h1>
+        Manage
+        <small>Expense</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="<?php echo base_url() . '/admin/dashboard'; ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="<?php echo base_url() . '/admin/expenses'; ?>"><i class="fa fa-dashboard"></i> Expense</a></li>
+        <li class="active">Manage</li>
+    </ol>
+</section>
+<section class="content">
 
-$this->breadcrumbs=array(
-	'Expenses'=>array('index'),
-	'Manage',
-);
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box box-info">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Search</h3>
+                </div>
+                <?php
+                Yii::app()->clientScript->registerScript('search', "
+						$('form.search-form').submit(function(){
+							$('#expenses-grid').yiiGridView('update', {
+								data: $(this).serialize()
+							});
+							return false;
+						});
+						");
+                $this->renderPartial('_search', array(
+                    'model' => $model,
+                ));
+                ?>
+            </div>
+            <div class="box">
+                <div class="box-header">
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <h3 class="box-title">Expense List</h3>
+                        </div>
+                        <div class="col-sm-4">
+                            <a href="<?php echo base_url() . '/admin/expenses/create'; ?>">
+                                <button class="btn btn-block btn-primary">Add Expense</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-body">
+                    <div class="dataTables_wrapper form-inline dt-bootstrap">
 
-$this->menu=array(
-	array('label'=>'List Expenses', 'url'=>array('index')),
-	array('label'=>'Create Expenses', 'url'=>array('create')),
-);
-
-Yii::app()->clientScript->registerScript('search', "
-$('.search-button').click(function(){
-	$('.search-form').toggle();
-	return false;
-});
-$('.search-form form').submit(function(){
-	$('#expenses-grid').yiiGridView('update', {
-		data: $(this).serialize()
-	});
-	return false;
-});
-");
-?>
-
-<h1>Manage Expenses</h1>
-
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
-
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
-
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'expenses-grid',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
-	'columns'=>array(
-		'id',
-		'detail',
-		'amount',
-		'status',
-		'deleted',
-		'created_by',
-		/*
-		'updated_by',
-		'date_entered',
-		'date_modified',
-		*/
-		array(
-			'class'=>'CButtonColumn',
-		),
-	),
-)); ?>
+                        <div class="row">
+                            <div class="col-sm-12 table-responsive">
+                                <?php
+                                $this->widget('zii.widgets.grid.CGridView', array(
+                                    'id' => 'expenses-grid',
+                                    'itemsCssClass' => 'table table-bordered table-hover dataTable',
+                                    'dataProvider' => $model->search(),
+                                    'enablePagination' => true,
+                                    // 'filter'=>$model,
+                                    'columns' => array(
+										'amount',
+										'expense_date'
+                                        array(
+                                            'class' => 'CButtonColumn',
+                                            'template' => '{v} {u} {d}', // <-- TEMPLATE WITH THE TWO STATES
+                                            'htmlOptions' => array(
+                                                'width' => 80,
+                                            ),
+                                            'buttons' => array(
+                                                'v' => array(
+                                                    'label' => '<i class="fa fa-search"></i>',
+                                                    'url' => 'Yii::app()->createUrl("admin/expenses/view", array("id"=>$data->id))',
+                                                    'options' => array('class' => 'view', 'title' => 'View'),
+                                                ),
+                                                'u' => array(
+                                                    'label' => '<i class="fa fa-edit"></i>',
+                                                    'url' => 'Yii::app()->createUrl("admin/expenses/update", array("id"=>$data->id))',
+                                                    'options' => array('class' => 'edit', 'title' => 'Update'),
+                                                ),
+                                                'd' => array(
+                                                    'label' => '<i class="fa fa-trash"></i>',
+                                                    'url' => 'Yii::app()->createUrl("admin/expenses/delete", array("id"=>$data->id))',
+                                                    'options' => array('class' => 'delete', 'title' => 'Delete'),
+                                                    'click' => 'function(){return confirm("are you sure ? Deleting this genre will delete all original and remix song under this genre");}'
+                                                ),
+                                            ),
+                                        )
+                                    ),
+                                ));
+                                ?>
+                            </div>
+                        </div>
+                        <div class="row"><div class="col-sm-6"></div><div class="col-sm-6"></div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
