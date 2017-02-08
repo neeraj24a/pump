115d0ffc39c3038f06ad126ecb5720b42459f690
neeraj24a <neeraj24a@gmail.com>
2017-02-08 17:37:05 +0530

    Expenses Search Form Update

--- protected/modules/admin/views/expenses/_search.php
+++ protected/modules/admin/views/expenses/_search.php
@@ -1,65 +1,18 @@
-<?php
-/* @var $this ExpensesController */
-/* @var $model Expenses */
-/* @var $form CActiveForm */
-?>
-
-<div class="wide form">
-
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'action'=>Yii::app()->createUrl($this->route),
 	'method'=>'get',
+	'htmlOptions'=>array('class' => 'search-form')
 )); ?>
-
-	<div class="row">
-		<?php echo $form->label($model,'id'); ?>
-		<?php echo $form->textField($model,'id',array('size'=>36,'maxlength'=>36)); ?>
+<div class="box-body">
+	<div class="form-group">
+		<div class="col-xs-4">
+			<?php echo $form->label($model,'expense_date'); ?>
+			<?php echo $form->textField($model,'expense_date',array('size'=>60,'maxlength'=>255,'class' => 'form-control')); ?>
+		</div>
 	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'detail'); ?>
-		<?php echo $form->textField($model,'detail',array('size'=>60,'maxlength'=>255)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'amount'); ?>
-		<?php echo $form->textField($model,'amount',array('size'=>16,'maxlength'=>16)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'status'); ?>
-		<?php echo $form->textField($model,'status'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'deleted'); ?>
-		<?php echo $form->textField($model,'deleted'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'created_by'); ?>
-		<?php echo $form->textField($model,'created_by',array('size'=>36,'maxlength'=>36)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'updated_by'); ?>
-		<?php echo $form->textField($model,'updated_by',array('size'=>36,'maxlength'=>36)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'date_entered'); ?>
-		<?php echo $form->textField($model,'date_entered'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'date_modified'); ?>
-		<?php echo $form->textField($model,'date_modified'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Search'); ?>
-	</div>
-
+</div>
+<div class="box-footer">
+	<?php echo CHtml::submitButton('Search',array("class" => 'btn btn-info search-button')); ?>
+	<a href="<?php echo base_url().'/admin/expenses/manage' ?>" class="btn btn-warning">Clear</a>
+</div>
 <?php $this->endWidget(); ?>
-
-</div><!-- search-form -->
