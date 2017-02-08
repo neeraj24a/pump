fe81b2e7d3eb90f01d80ce4199702a6e0ce80135
neeraj24a <neeraj24a@gmail.com>
2017-02-08 17:45:50 +0530

    Update Oil Price view

--- protected/modules/admin/views/oilPrice/view.php
+++ protected/modules/admin/views/oilPrice/view.php
@@ -1,34 +1,49 @@
-<?php
-/* @var $this OilPriceController */
-/* @var $model OilPrice */
+<section class="content-header">
+    <h1>
+        Expense
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="<?php echo base_url() . '/admin/dashboard'; ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="<?php echo base_url() . '/admin/expenses'; ?>"><i class="fa fa-dashboard"></i> Expense</a></li>
+        <li class="active">View</li>
+    </ol>
+</section>
+<section class="content">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box box-info">
+                <div class="box-header with-border">
+                    <h3 class="box-title">
+					    <?php 
+							$model->product_type = ProductType::model()->findByPk($model->product_type)->name;
+							echo $model->product_type; 
+						?> 
+                        <small>
+                            <a href="<?php echo base_url() . '/admin/expenses/update?id=' . $model->id; ?>">EDIT</a>
+                        </small>
+                    </h3>
+                </div>
+                <div class="box-body">
+                    <div class="col-xs-12 table-responsive">
+                        <?php
+						$model->product_type = ProductType::model()->findByPk($model->product_type)->name;
+                        $this->widget('zii.widgets.CDetailView', array(
+                            // 'itemsCssClass' => 'table table-bordered table-hover dataTable',
+                            'htmlOptions' => array("class" => "table table-bordered table-hover dataTable"),
+                            'data' => $model,
+                            'attributes' => array(
+                                'product_type',
+								'price',
+							),
+                        ));
+                        ?>
+                        <div class="col-xs-12">
+                            <?php echo CHtml::link('Back', array('/admin/expenses'), array("class" => 'btn btn-info pull-right', "style" => "margin-left:10px;")); ?>
+                        </div>
+                    </div>
 
-$this->breadcrumbs=array(
-	'Oil Prices'=>array('index'),
-	$model->id,
-);
-
-$this->menu=array(
-	array('label'=>'List OilPrice', 'url'=>array('index')),
-	array('label'=>'Create OilPrice', 'url'=>array('create')),
-	array('label'=>'Update OilPrice', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete OilPrice', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage OilPrice', 'url'=>array('admin')),
-);
-?>
-
-<h1>View OilPrice #<?php echo $model->id; ?></h1>
-
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'product_type',
-		'price',
-		'status',
-		'deleted',
-		'created_by',
-		'updated_by',
-		'date_entered',
-		'date_modified',
-	),
-)); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
