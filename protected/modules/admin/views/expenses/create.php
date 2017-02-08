79430c691763d3b4b9c3c9882660cfe5ea3f9274
neeraj24a <neeraj24a@gmail.com>
2017-02-08 17:11:43 +0530

    Expenses Update View

--- protected/modules/admin/views/expenses/create.php
+++ protected/modules/admin/views/expenses/create.php
@@ -1,18 +1,23 @@
-<?php
-/* @var $this ExpensesController */
-/* @var $model Expenses */
-
-$this->breadcrumbs=array(
-	'Expenses'=>array('index'),
-	'Create',
-);
-
-$this->menu=array(
-	array('label'=>'List Expenses', 'url'=>array('index')),
-	array('label'=>'Manage Expenses', 'url'=>array('admin')),
-);
-?>
-
-<h1>Create Expenses</h1>
-
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<section class="content-header">
+  <h1>
+    Add
+    <small>Expense</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="<?php echo base_url() . '/admin/dashboard'; ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+    <li><a href="<?php echo base_url() . '/admin/expenses'; ?>"><i class="fa fa-dashboard"></i> Expenses</a></li>
+    <li class="active">Add</li>
+</ol>
+</section>
+<section class="content">
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="box box-info">
+				<div class="box-header with-border">
+					<h3 class="box-title">Daily Expense</h3>
+				</div>
+				<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+			</div>
+		</div>
+	</div>
+</section>
