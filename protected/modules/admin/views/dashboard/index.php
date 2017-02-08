1653590c4cb30de33d120b70f84d5744724773cc
neeraj24a <neeraj24a@gmail.com>
2017-02-08 18:37:08 +0530

    Dashboard Chart Plot

--- protected/modules/admin/views/dashboard/index.php
+++ protected/modules/admin/views/dashboard/index.php
@@ -1,3 +1,5 @@
+<script src="<?php echo base_url(); ?>/themes/admin/plugins/highcharts.js"></script>
+<script src="<?php echo base_url(); ?>/themes/admin/plugins/exporting.js"></script>
 <section class="content-header">
     <h1>
         Dashboard
@@ -20,3 +22,69 @@
         </div>
     </div>     
 </section>
+<script>
+    Highcharts.chart('container', {
+    chart: {
+        type: 'area',
+        inverted: true
+    },
+    title: {
+        text: 'Average fruit consumption during one week'
+    },
+    subtitle: {
+        style: {
+            position: 'absolute',
+            right: '0px',
+            bottom: '10px'
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'top',
+        x: -150,
+        y: 100,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+    },
+    xAxis: {
+        categories: [
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+            'Sunday'
+        ]
+    },
+    yAxis: {
+        title: {
+            text: 'Number of units'
+        },
+        labels: {
+            formatter: function () {
+                return this.value;
+            }
+        },
+        min: 0
+    },
+    plotOptions: {
+        area: {
+            fillOpacity: 0.5
+        }
+    },
+    series: [{
+        name: 'Petrol',
+        data: [300, 400, 300, 500, 400, 1000, 1200]
+    }, {
+        name: 'Diesel',
+        data: [800, 1000, 400, 1300, 1300, 500, 400]
+    }, {
+        name: 'Mobil',
+        data: [10, 30, 40, 30, 30, 50, 40]
+    }
+     ]
+});
+</script>
