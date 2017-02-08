de8708a41fa296f8dedcd061efe8d8007f20add5
neeraj24a <neeraj24a@gmail.com>
2017-02-08 17:14:34 +0530

    Oil Purchase Update View

--- protected/modules/admin/views/oilPrice/update.php
+++ protected/modules/admin/views/oilPrice/update.php
@@ -1,21 +1,23 @@
-<?php
-/* @var $this OilPriceController */
-/* @var $model OilPrice */
-
-$this->breadcrumbs=array(
-	'Oil Prices'=>array('index'),
-	$model->id=>array('view','id'=>$model->id),
-	'Update',
-);
-
-$this->menu=array(
-	array('label'=>'List OilPrice', 'url'=>array('index')),
-	array('label'=>'Create OilPrice', 'url'=>array('create')),
-	array('label'=>'View OilPrice', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage OilPrice', 'url'=>array('admin')),
-);
-?>
-
-<h1>Update OilPrice <?php echo $model->id; ?></h1>
-
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<section class="content-header">
+  <h1>
+    Update
+    <small>Oil Price</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="<?php echo base_url() . '/admin/dashboard'; ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+    <li><a href="<?php echo base_url() . '/admin/oilPrice'; ?>"><i class="fa fa-dashboard"></i> Oil Price</a></li>
+    <li class="active">Update</li>
+</ol>
+</section>
+<section class="content">
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="box box-info">
+				<div class="box-header with-border">
+					<h3 class="box-title"><?php echo ProductType::model()->findByPk($model->product_type)->name; ?></h3>
+				</div>
+				<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+			</div>
+		</div>
+	</div>
+</section>
